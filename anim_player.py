d09dbe8daba4eff44ba3312192730a5286234d15
hansen1416 <badapplesweetie@gmail.com>
2025-06-07 18:29:42 +0100

    1

---
+++ anim_player.py
@@ -0,0 +1,90 @@
+import os
+import time
+
+# from abc import ABC, abstractmethod
+
+import cv2
+import numpy as np
+import open3d as o3d
+import open3d.visualization.gui as gui
+import open3d.visualization.rendering as rendering
+from smplx import SMPL, SMPLX, MANO, FLAME
+
+from utils.utils import (
+    get_checkerboard_plane,
+)
+
+
+class AnimPlayer:
+
+    def __init__(self):
+
+        # We need to initalize the application, which finds the necessary shaders
+        # for rendering and prepares the cross-platform window abstraction.
+        gui.Application.instance.initialize()
+
+        width, height = 1920, 1080
+
+        self.window = gui.Application.instance.create_window("Open3D", width, height)
+
+        # 3D widget
+        self._scene = gui.SceneWidget()
+        self._scene.scene = rendering.Open3DScene(self.window.renderer)
+        self.window.add_child(self._scene)
+
+        self._setup_lighting()
+        self._add_ground()
+        self._set_camera()
+
+    def _setup_lighting(self):
+        self._scene.scene.set_background([0.96, 0.94, 0.91, 1])  # white background
+
+        self._scene.scene.scene.set_sun_light(
+            [-0.577, -0.577, -0.577],  # direction
+            [1.0, 1.0, 1.0],  # color
+            50000,  # intensity
+        )
+        self._scene.scene.scene.enable_sun_light(True)
+
+    def _add_ground(self):
+        gp = get_checkerboard_plane(plane_width=2, num_boxes=9)
+
+        for idx, g in enumerate(gp):
+            g.compute_vertex_normals()
+            self._scene.scene.add_geometry(
+                f"__ground_{idx:04d}__", g, rendering.MaterialRecord()
+            )
+
+    def _set_camera(self):
+        center = [0, 0, 0]  # center of the ground plane
+        eye = [0, 1.0, 2.0]  # slightly above and behind
+        up = [0, 1, 0]
+
+        bbox = o3d.geometry.AxisAlignedBoundingBox(
+            min_bound=[-1, -1, -1], max_bound=[1, 1, 1]
+        )
+        self._scene.setup_camera(60.0, bbox, center)
+
+        # Move camera position manually
+        self._scene.scene.camera.look_at(center, eye, up)
+
+    def load_smpl_mesh(self, verts, faces):
+
+        mesh = o3d.geometry.TriangleMesh()
+
+        mesh.vertices = o3d.utility.Vector3dVector(verts)
+        mesh.triangles = o3d.utility.Vector3iVector(faces)
+        mesh.compute_vertex_normals()
+        mesh.paint_uniform_color([0.5, 0.5, 0.5])
+
+        return mesh
+
+    def run(self):
+        gui.Application.instance.run()
+
+
+if __name__ == "__main__":
+
+    animPlayer = AnimPlayer()
+
+    animPlayer.run()
